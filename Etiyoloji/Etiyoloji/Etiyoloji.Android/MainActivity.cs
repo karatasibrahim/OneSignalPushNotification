c32545787d2c65e75371009b5ca9df43f449cdf8
ibrahim karataş <ibrahimkaratas@windowslive.com>
2021-06-03 19:15:34 +0300

    Xamarin WebView Push Notification App
    Xamarin Form WebView App and OneSignal Push Notification

---
+++ Etiyoloji/Etiyoloji/Etiyoloji.Android/MainActivity.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.OS;
+using Com.OneSignal;
+
+namespace Etiyoloji.Droid
+{
+    [Activity(Label = "Etiyoloji", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            OneSignal.Current.StartInit("ID").EndInit();
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+            LoadApplication(new App());
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Etiyoloji/Etiyoloji/Etiyoloji/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Com.OneSignal;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Etiyoloji
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            OneSignal.Current.StartInit("ID").EndInit();
+        }
+
+        protected override void OnStart()
+        {
+            OneSignal.Current.RegisterForPushNotifications();
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
